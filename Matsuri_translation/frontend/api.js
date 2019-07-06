078953909862cecc6c5c307185f99ce757b8bf34
fzxiao233 <fzxiao@outlook.com>
2019-07-06 19:49:00 +0800

    update frontend

--- Matsuri_translation/frontend/api.js
+++ Matsuri_translation/frontend/api.js
@@ -2,7 +2,7 @@ function submit_task() {
     var url = $('#url').val();
     var translation = $('#translation').val();
     var jqxhr = $.ajax({
-        url: "http://127.0.0.1:5000/api/tasks",
+        url: "http://api.matsuri.design/api/tasks",
         type: "post",
         data: JSON.stringify({
             "url": url,
@@ -17,14 +17,14 @@ function submit_task() {
 
 function fetch_img(task_id) {
     var event = setInterval(function () {
-        var jqxhr = $.getJSON('http://127.0.0.1:5000/api/get_task=' + task_id,
+        var jqxhr = $.getJSON('http://api.matsuri.design/api/get_task=' + task_id,
             function (data) {
                 if (data.state === "SUCCESS") {
-                    $("#img").append('<img src="cache/' + data.result + '.png" alt="Result">')
+                    $("#img").append('<img src="cache/' + data.result + '.png" alt="Result" style:"">')
                     clearInterval(event);
                 }
             })
-    }, 1000)
+    }, 3000)
 }
 
 $(function () {
@@ -32,9 +32,3 @@ $(function () {
         submit_task();
     });
 });
-
-//我寻思post不是不能提交json吗？
-
-//额，其实想怎么搞都可以？ post就是个简短的ajax
-
-//你写，我学～可还行，我先本地跑起来看看ww
