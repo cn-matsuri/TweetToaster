28a96df3c2a9deb0a16fd82f21ae9ab6ff182888
Ubuntu <ubuntu@ip-172-26-12-64.ap-northeast-1.compute.internal>
2019-07-07 23:54:42 +0000

    部署脚本

---
+++ Matsuri_translation/celery_run.sh
@@ -0,0 +1,1 @@
+celery -A api.celery worker --loglevel info
--- Matsuri_translation/manger.py
+++ Matsuri_translation/manger.py
@@ -2,36 +2,34 @@ from celery import Celery, Task
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import Options
 
-from .tweet_process import TweetProcess
+from tweet_process import TweetProcess
 
-celery = Celery('tasks', broker='redis://localhost:6379', backend='redis://localhost:6379/0')
-
-
-# class BaseDriveTask(Task):
-#     _driver = None
-#
-#     @property
-#     def driver(self):
-#         if self._driver is None:
-#             chrome_options = Options()
-#             # chrome_options.add_argument("--headless")
-#             chrome_options.add_argument("--proxy-server=127.0.0.1:12333")
-#             self._driver = webdriver.Chrome(options=chrome_options)
-#         return self._driver
+celery = Celery('api', broker='redis://localhost:6379', backend='redis://localhost:6379/0')
 
 
-@celery.task()
-def execute_event(event):
-    chrome_options = Options()
-    # chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--proxy-server=127.0.0.1:12333")
-    driver = webdriver.Chrome(options=chrome_options)
+class BaseDriveTask(Task):
+    _driver = None
+
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--proxy-server=127.0.0.1:12333")
+        if self._driver is None:
+            self.driver = webdriver.Chrome(options=chrome_options)
+
+    def run(self, *args, **kwargs):
+        pass
+
+
+@celery.task(bind=True, base=BaseDriveTask)
+def execute_event(self, event):
     try:
-        processor = TweetProcess(driver)
+        processor = TweetProcess(self.driver)
         processor.open_page(event['url'])
         processor.modify_tweet(event['translation'])
         processor.scroll_page_to_tweet()
         filename = processor.save_screenshots()
     finally:
-        driver.close()
+        # self.driver.close()
+        pass
     return filename
