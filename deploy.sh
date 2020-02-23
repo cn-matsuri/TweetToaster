ba68f159077ac2ca5185d4f477ca6a5dc69e2ffa
FZxiao <42691214+fzxiao233@users.noreply.github.com>
2020-02-23 13:39:48 +0800

    Revert "Make deploy.sh safe and use-able"

--- deploy.sh
+++ deploy.sh
@@ -1,39 +1,14 @@
 #!/usr/bin/env bash
-
-command_exists()
-{
-  command -v "$1" >/dev/null 2>&1
-}
-
-if [ $(command_exists git) ] && \
-   [ $(command_exists python) ] && \
-   [ $(command_exists pip) ] && \
-   [ $(command_exists aaa) ]; then
-    echo "Dependency Check OK";
-else
-    echo 'Warning: whether python or pip or git is not installed.';
-    echo 'Try installing automaticly...';
-    # Ask for sudo privileges
-    [ "$UID" -eq 0 ] || exec sudo "$0" "$@";
-    
-    if [ -n "$(uname -a | grep Ubuntu)" ]; then
-	sudo apt-get update && sudo apt-get install git python python-pip;
-    elif [ -n "$(uname -a | grep CentOS)" ]; then
-	sudo yum update && sudo yum install git python python-pip;
-    else
-	echo 'Error: Not a supported platform, please install dependencies by yourself!';
-	exit 1;
-    fi
-fi
-
+sudo echo "$USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers
+sudo apt install python python-pip git
 pip install fabric
 git clone https://github.com/cn-matsuri/matsuri_translation
 cd matsuri_translation
 python deploy.py
-domain=''
+domain = ''
 echo 'Input your domain to configured nginx, or you shall edit /etc/nginx/conf.d/1-matsuri_translation.conf'
 read domain
-sudo echo "server {
+sudo echo"server {
     listen 80;
     server_name $domain;
     location ^~ /api/ {
