e112e278a0cfb3e89378b4f475a4554f1e655129
yuyuyzl <yuzhongliang22@163.COM>
2020-06-04 13:45:06 +0800

    修复安卓上下载变白问题

--- Matsuri_translation/frontend/api3.js
+++ Matsuri_translation/frontend/api3.js
@@ -371,8 +371,32 @@ function getUrlParam(k) {
     }
 }
 
-$(function () {
+function loadJS(url, callback) {
+    var script = document.createElement('script'),
+        fn = callback || function () {
+        };
+    script.type = 'text/javascript';
+    if (script.readyState) {
+        script.onreadystatechange = function () {
+            if (script.readyState == 'loaded' || script.readyState == 'complete') {
+                script.onreadystatechange = null;
+                fn();
+            }
+        }
+    } else {
+        script.onload = function () {
+            fn();
+        };
+    }
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
 
+if (getUrlParam('debug'))
+    loadJS("https://cdn.bootcdn.net/ajax/libs/vConsole/3.3.4/vconsole.min.js", () => {
+        new VConsole();
+    });
+$(function () {
     if (getUrlParam("template") != null && getUrlParam("template").length > 0 && getUrlParam("out") == null) {
         $.get(getUrlParam("template"), function (data, status) {
             console.log(data);
