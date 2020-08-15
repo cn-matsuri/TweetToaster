903acfc0471a16ebab08849e64e306dc381c45d7
yuyuyzl <yuzhongliang22@163.COM>
2020-08-16 02:59:53 +0800

    feat: keep chrome awake and twitter loaded

--- Matsuri_translation/celeryconfig_example.py
+++ Matsuri_translation/celeryconfig_example.py
@@ -3,3 +3,11 @@ broker_url = 'redis://localhost:6379/0'
 result_backend = 'redis://localhost:6379/0'
 
 self_url = 'http://localhost/'
+
+# 新特性加速，使用前咨询鱼鱼
+# task_routes = {'Matsuri_translation.manager.execute_event': 'twitter',
+#                 'Matsuri_translation.manager.execute_event_auto': "auto"
+#                 }
+#
+# chrome_twitter_port=range(9222,9224)
+# chrome_auto_port=range(9224,9226)
